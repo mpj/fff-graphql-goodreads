5303890c90347d182b7b73106be24ac8b5ccb908
Mattias Petter Johansson <mpj@mpj.me>
2017-09-22 17:43:34 +0200

    added translations

--- schema.js
+++ schema.js
@@ -9,6 +9,29 @@ const {
   GraphQLList
 } = require('graphql')
 
+function translate(lang, str) {
+  // Google Translate API is a paid (but dirt cheap) service. This is my key
+  // and will be disabled by the time the video is out. To generate your own,
+  // go here: https://cloud.google.com/translate/v2/getting_started
+  const apiKey =
+    'AIzaSyBN-bwtos8sKU6X84wkrdjtCF7uzng6kgQ'
+	const url =
+    'https://www.googleapis.com' +
+    '/language/translate/v2' +
+  	'?key=' + apiKey +
+    '&source=en' +
+    '&target=' + lang +
+    '&q=' + encodeURIComponent(str)
+  return fetch(url)
+ 		.then(response => response.json())
+	  .then(parsedResponse =>
+    	parsedResponse
+      	.data
+        .translations[0]
+        .translatedText
+    )
+}
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   description: '...',
@@ -16,7 +39,13 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     title: {
       type: GraphQLString,
-      resolve: xml => xml.GoodreadsResponse.book[0].title[0]
+      args: {
+        lang: { type: GraphQLString }
+      },
+      resolve: (xml, args) => {
+        const title = xml.GoodreadsResponse.book[0].title[0]
+        return args.lang ? translate(args.lang, title) : title
+      }
     },
     isbn: {
       type: GraphQLString,
