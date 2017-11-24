11bf651a06b24b04cce213e0ad5f67dcc8826d0e
Aditya Agarwal <adityaa803@gmail.com>
2017-11-24 15:54:16 +0530

    Use es6 string template

--- schema.js
+++ schema.js
@@ -10,15 +10,8 @@ function translate(lang, str) {
   // Google Translate API is a paid (but dirt cheap) service. This is my key
   // and will be disabled by the time the video is out. To generate your own,
   // go here: https://cloud.google.com/translate/v2/getting_started
-  const apiKey =
-    'AIzaSyBN-bwtos8sKU6X84wkrdjtCF7uzng6kgQ'
-	const url =
-    'https://www.googleapis.com' +
-    '/language/translate/v2' +
-  	'?key=' + apiKey +
-    '&source=en' +
-    '&target=' + lang +
-    '&q=' + encodeURIComponent(str)
+  const apiKey = 'AIzaSyBN-bwtos8sKU6X84wkrdjtCF7uzng6kgQ'
+  const url = `https://www.googleapis.com/language/translate/v2?key=${apiKey}&source=en&target=${lang}&q=${encodeURIComponent(str)`
   return fetch(url)
  		.then(response => response.json())
 	  .then(parsedResponse =>
