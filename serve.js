c0d604e93fafac652f8485811ab09af6c6aa4234
Ali Günenç <alig@alig-macos.local>
2019-11-10 01:06:22 +0300

    Listening app with nodemon

--- serve.js
+++ serve.js
@@ -6,34 +6,32 @@ const schema = require('./schema')
 const DataLoader = require('dataloader')
 const util = require('util')
 const parseXML = util.promisify(require('xml2js').parseString)
+const port = 3000;
+
+const apiKey = '[YOUR GODREADS API KEY]';
 
 const fetchAuthor = id =>
-fetch(`https://www.goodreads.com/author/show.xml?id=${id}&key=Rz607gLuIzr1a55wEbl3w `)
-.then(response => response.text())
-.then(parseXML)
+  fetch(`https://www.goodreads.com/author/show.xml?id=${id}&key=${apiKey}`)
+    .then(response => response.text())
+    .then(parseXML);
 
 const fetchBook = id =>
-fetch(`https://www.goodreads.com/book/show/${id}.xml?key=Rz607gLuIzr1a55wEbl3w`)
-  .then(response => response.text())
-  .then(parseXML)
+  fetch(`https://www.goodreads.com/book/show/${id}.xml?key=${apiKey}`)
+    .then(response => response.text())
+    .then(parseXML);
 
-app.use('/graphql', graphqlHTTP( req => {
-
-  const authorLoader = new DataLoader(keys =>
-    Promise.all(keys.map(fetchAuthor)))
-
-  const bookLoader = new DataLoader(keys =>
-    Promise.all(keys.map(fetchBook)))
+app.use('/graphql', graphqlHTTP(req => {
+  const context = {
+    authorLoader: new DataLoader(keys => Promise.all(keys.map(fetchAuthor))),
+    bookLoader: new DataLoader(keys => Promise.all(keys.map(fetchBook)))
+  };
 
   return {
     schema,
-    context: {
-      authorLoader,
-      bookLoader
-    },
+    context,
     graphiql: true
   }
 }))
 
-app.listen(4000)
-console.log('Listening ...')
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
