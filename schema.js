780ebdf12da306be158565659633391acca4b7f9
Ali Günenç <alig@alig-macos.local>
2019-11-10 01:05:59 +0300

    Add merge schemas feature

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const { mergeSchemas } = require('graphql-tools')
 const {
   GraphQLSchema,
   GraphQLObjectType,
@@ -6,42 +7,13 @@ const {
   GraphQLList
 } = require('graphql')
 
-function translate(lang, str) {
-  // Google Translate API is a paid (but dirt cheap) service. This is my key
-  // and will be disabled by the time the video is out. To generate your own,
-  // go here: https://cloud.google.com/translate/v2/getting_started
-  const apiKey =
-    'AIzaSyBN-bwtos8sKU6X84wkrdjtCF7uzng6kgQ'
-	const url =
-    'https://www.googleapis.com' +
-    '/language/translate/v2' +
-  	'?key=' + apiKey +
-    '&source=en' +
-    '&target=' + lang +
-    '&q=' + encodeURIComponent(str)
-  return fetch(url)
- 		.then(response => response.json())
-	  .then(parsedResponse =>
-    	parsedResponse
-      	.data
-        .translations[0]
-        .translatedText
-    )
-}
-
 const BookType = new GraphQLObjectType({
   name: 'Book',
-  description: '...',
-
   fields: () => ({
     title: {
       type: GraphQLString,
-      args: {
-        lang: { type: GraphQLString }
-      },
-      resolve: (xml, args) => {
-        const title = xml.GoodreadsResponse.book[0].title[0]
-        return args.lang ? translate(args.lang, title) : title
+      resolve: xml => {
+        return xml.GoodreadsResponse.book[0].title[0];
       }
     },
     isbn: {
@@ -51,8 +23,8 @@ const BookType = new GraphQLObjectType({
     authors: {
       type: new GraphQLList(AuthorType),
       resolve: (xml, args, context) => {
-        const authorElements = xml.GoodreadsResponse.book[0].authors[0].author
-        const ids = authorElements.map(elem => elem.id[0])
+        const ids = xml.GoodreadsResponse.book[0].authors[0].author.map(elem => elem.id[0])
+
         return context.authorLoader.loadMany(ids)
       }
     }
@@ -61,29 +33,25 @@ const BookType = new GraphQLObjectType({
 
 const AuthorType = new GraphQLObjectType({
   name: 'Author',
-  description: '...',
-
   fields: () => ({
     name: {
       type: GraphQLString,
-      resolve: xml =>
-        xml.GoodreadsResponse.author[0].name[0]
+      resolve: xml => xml.GoodreadsResponse.author[0].name[0]
     },
     books: {
       type: new GraphQLList(BookType),
-      resolve: (xml, args, context) => {
-        const ids = xml.GoodreadsResponse.author[0].books[0].book.map(elem => elem.id[0]._)
+      resolve: (xml, arg, context) => {
+        const ids = xml.GoodreadsResponse.author[0].books[0].book.map(elem => elem && elem.id[0]._)
+
         return context.bookLoader.loadMany(ids)
       }
     }
   })
 })
 
-module.exports = new GraphQLSchema({
+const AuthorSchema = new GraphQLSchema({
   query: new GraphQLObjectType({
-    name: 'Query',
-    description: '...',
-
+    name: 'AuthorQuery',
     fields: () => ({
       author: {
         type: AuthorType,
@@ -95,3 +63,27 @@ module.exports = new GraphQLSchema({
     })
   })
 })
+
+const BookSchema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'BookQuery',
+    fields: () => ({
+      book: {
+        type: BookType,
+        args: {
+          id: { type: GraphQLInt }
+        },
+        resolve: (root, args, context) => context.bookLoader.load(args.id)
+      }
+    })
+  })
+})
+
+const schemas = mergeSchemas({
+  schemas: [
+    AuthorSchema,
+    BookSchema
+  ]
+});
+
+module.exports = schemas
